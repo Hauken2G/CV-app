4ddd1b923c6df9961c3f3b615b29cec5ef9b7ca7
Hauken2G <kostasmichopoulos2@gmail.com>
2024-12-03 16:12:57 +0200

    cv app start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.css';
+import { ImagePicker } from './components';
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <ImagePicker />
+      </div>
+    </>
+  );
+}
